464f3be117bf33f2f2c3ce9489281ba27ca512a9
Xiaoyu <xiaoyubai2002@yahoo.de>
2018-05-24 14:58:23 +0200

    info on embedding coverage

---
+++ Resources/vocab_coverage.py
@@ -0,0 +1,59 @@
+import sys
+import re
+import json
+import gensim.models as gm
+from nltk.tokenize import word_tokenize
+
+with open('../Data/germeval2018.training.txt', 'r', encoding='utf-8') as fi:
+    content = [line.strip().split('\t')[0] for line in fi]
+# print(content[:5])
+
+# clean data
+vocab = set()
+for tw in content:
+    tw = re.sub(r'@\S+','User', tw)
+    tw = re.sub(r'\|LBR\|', '', tw)
+    tw = re.sub(r'#', '', tw)
+    vocab.update([ token.lower() for token in word_tokenize(tw)])
+
+print('{} unique tokens in training data'.format(len(vocab)))
+# print(vocab)
+
+path_embed = sys.argv[1]
+if path_embed.endswith('json'):
+    f = open(path_embed, 'rb')
+    embeds = json.load(f)
+    f.close
+    embed_vocab = {k for k,v in embeds.items()}
+elif path_embed.endswith('bin'):
+    embeds = gm.KeyedVectors.load(path_embed).wv
+    embed_vocab = {word for word in embeds.index2word}
+
+print('Vocab size of embeddings: {}'.format(len(embed_vocab)))
+
+###### Checking coverage of embeddings w.r.t. our training data #########
+Ivocab = vocab.intersection(embed_vocab)
+OOV = vocab - Ivocab
+frac_in = round(len(Ivocab)/len(vocab),2)
+frac_out = round(len(OOV)/len(vocab),2)
+
+print('''Coverage of {}:
+Words in embeddings: {}, {} of total training data
+Words not in embeddings (OOV): {}, {} of total training data'''.format(path_embed, len(Ivocab), frac_in, len(OOV), frac_out))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## space ##
